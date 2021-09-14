b56e7e6b7ab355c465bc2836dec8cec1f78140e3
SebastianMera <sebi.mera@ymail.com>
2021-09-14 15:06:54 +0300

    branch initialization

--- moody/js/src/brain/index.js
+++ moody/js/src/brain/index.js
@@ -1,20 +1,23 @@
-import { MAX_FACE_DETECTION_WIDTH, MAX_FACE_DETECTION_HEIGHT } from '../constants';
+import {
+  MAX_FACE_DETECTION_WIDTH,
+  MAX_FACE_DETECTION_HEIGHT,
+} from '../constants';
 import moody from '../output/moody';
 
 const char = moody('#vis');
 const { opts, lookAt } = char;
 
-
 function main(input) {
   const { faces } = input;
-
+  console.log(input);
   if (faces && faces.length > 0) {
-    const { boundingBox: {x, y} } = faces[0];
-    const lookAtX = x * 1000 / MAX_FACE_DETECTION_WIDTH;
-    const lookAtY = y * 400 / MAX_FACE_DETECTION_HEIGHT;
+    const {
+      boundingBox: { x, y },
+    } = faces[0];
+    const lookAtX = (x * 1000) / MAX_FACE_DETECTION_WIDTH;
+    const lookAtY = (y * 400) / MAX_FACE_DETECTION_HEIGHT;
     char.lookAt(lookAtX, lookAtY);
   }
 }
-
 
 export default main;
